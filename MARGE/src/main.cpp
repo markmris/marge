8e070d15d07b14fbeb54153c4adcfa49ee9510a7
markmris <mark.mrishih@gmail.com>
2026-03-30 18:15:40 -0300

    Create vector3 struct, operator overload for addition

--- MARGE/src/main.cpp
+++ MARGE/src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+
+struct vector3
+{
+	float x, y, z;
+
+	vector3() : x(0), y(0), z(0) {}
+	vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+
+	vector3 operator +(const vector3& v) const
+	{
+		return vector3(x + v.x, y + v.y, z + v.z);
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	
+
+	return 0;
 }
