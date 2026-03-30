722597ba3b9597c894e79c72fc005540f14bc6ed
markmris <mark.mrishih@gmail.com>
2026-03-30 19:38:23 -0300

    Added all operations for vector3 struct

--- MARGE/src/main.cpp
+++ MARGE/src/main.cpp
@@ -8,15 +8,35 @@ struct vector3
 	vector3() : x(0), y(0), z(0) {}
 	vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 
-	vector3 operator +(const vector3& v) const
+	vector3 operator +(const vector3 &v) const
 	{
 		return vector3(x + v.x, y + v.y, z + v.z);
 	}
+
+	vector3 operator -(const vector3 &v) const
+	{
+		return vector3(x - v.x, y - v.y, z - v.z);
+	}
+
+	vector3 operator *(const float &n) const
+	{
+		return vector3(x * n, y * n, z * n);
+	}
+
+	vector3 operator /(const float& n) const
+	{
+		return vector3(x / n, y / n, z / n);
+	}
 };
+
+// Handle reversed vector multiplication
+inline vector3 operator *(const float n, const vector3 v)
+{
+	return v * n;
+}
 
 int main()
 {
-	
 
 	return 0;
 }
