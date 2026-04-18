b622f0a3087dfdd8d4b82be5286c234ee51f358e
markmris <mark.mrishih@gmail.com>
2026-04-18 11:47:23 -0300

    Added progress display

--- MARGE/src/main.cpp
+++ MARGE/src/main.cpp
@@ -69,13 +69,14 @@ int main()
 	int width = 800;
 	int height = 800;
 
-
 	std::cout << "P3\n" << width << ' ' << height << "\n255\n";
 
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++) 
 		{
+			std::clog << "\rScanlines remaining" << (height - j) << std::flush;
+
 			auto r = float(j) / (width - 1);
 			auto g = float(i) / (height - 1);
 			auto b = 0;
@@ -87,7 +88,7 @@ int main()
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
-
+	std::clog << "\rDONE----------------------\n";
 
 	return 0;
 }
