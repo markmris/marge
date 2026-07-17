e6d0dd48f2a40f117f1b45a84ece879380e557c5
markmris <mark.mrishih@gmail.com>
2026-07-17 19:42:31 -0300

    Delete marge.cpp and keep marge.h

--- MARGE/src/includes/marge.cpp
+++
@@ -1,9 +0,0 @@
-// #include "marge.h"
-
-// const double infinity = std::numeric_limits<double>::infinity();
-// const double pi = 3.1415926535897932385;
-
-// inline double degAsRad(double degrees)
-// {
-//     return (degrees * pi) / 180;
-// }
