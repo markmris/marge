db60d2e6b2e592574838920d9f0cbe6e58d72977
markmris <mark.mrishih@gmail.com>
2026-07-17 19:50:30 -0300

    Add random number generation for real numbers in [0, 1) and real numbers in [min, max) where min and max are custom

--- MARGE/src/includes/marge.h
+++ MARGE/src/includes/marge.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 #include <memory>
 #include <limits>
 
@@ -19,4 +20,16 @@ const double pi = 3.1415926535897932385;
 inline double degAsRad(double degrees)
 {
     return (degrees * pi) / 180;
+}
+
+// Return a random real number in [0, 1)
+inline double randomDouble()
+{
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+// Return a random real number in [min, max)
+inline double randomDouble(double min, double max)
+{
+    return min + (max - min) * randomDouble();
 }
