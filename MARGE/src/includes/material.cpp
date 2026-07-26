a73b5b9e9ff022b2ed646f073880d4dd28e1aebc
markmris <mark.mrishih@gmail.com>
2026-07-25 23:55:17 -0300

    Tighten diffuse::scatter for near-zero vector3 edge cases

--- MARGE/src/includes/material.cpp
+++ MARGE/src/includes/material.cpp
@@ -10,6 +10,10 @@ diffuse::diffuse(const color3& albedo) : albedo(albedo) {}
 bool diffuse::scatter(const ray& rayIn, const hitdata& hd, color3& attenuation, ray& scattered) const override
 {
 	vector3 scatterDirection = hd.normal + randomNormalVector();
+
+	if (scatterDirection.nearZero())
+		scatterDirection = hd.normal;
+
 	scattered = ray(hd.point, scatterDirection);
 	attenuation = albedo;
 
