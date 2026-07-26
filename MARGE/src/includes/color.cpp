8bc03468c32f5e6117a7f81775f7fb61868d2342
markmris <mark.mrishih@gmail.com>
2026-07-25 23:13:11 -0300

    Implement Gamma Correction for accurate color intensity

--- MARGE/src/includes/color.cpp
+++ MARGE/src/includes/color.cpp
@@ -1,12 +1,20 @@
 #include <algorithm>
+#include <cmath>
 #include "color.h"
-#include "interval.h"
 
 void writeColor(std::ostream& out, const color3& color)
 {
-    int rbyte = std::clamp(int(255.999 * color.x), 0, 255);
-    int gbyte = std::clamp(int(255.999 * color.y), 0, 255);
-    int bbyte = std::clamp(int(255.999 * color.z), 0, 255);
+    int rbyte = std::clamp(int(255.999 * linearToGamma(color.x)), 0, 255);
+    int gbyte = std::clamp(int(255.999 * linearToGamma(color.y)), 0, 255);
+    int bbyte = std::clamp(int(255.999 * linearToGamma(color.z)), 0, 255);
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+double linearToGamma(double linearComponent)
+{
+    if (linearComponent > 0)
+        return std::sqrt(linearComponent);
+
+    return 0;
 }
--- MARGE/src/includes/color.h
+++ MARGE/src/includes/color.h
@@ -5,3 +5,4 @@
 using color3 = vector3;
 
 void writeColor(std::ostream& out, const color3 &color);
+double linearToGamma(double linearComponent);
