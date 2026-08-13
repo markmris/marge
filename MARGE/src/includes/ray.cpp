85edbccb63efeb226837641232c7a8064480fcbe
markmris <mark.mrishih@gmail.com>
2026-08-13 19:54:32 -0300

    Add moving sphere constructor

--- MARGE/src/includes/ray.cpp
+++ MARGE/src/includes/ray.cpp
@@ -2,7 +2,7 @@
 
 ray::ray() : origin(point3(0, 0, 0)), direction(vector3(0, 0, 0)), time(0) {};
 ray::ray(const point3& origin, const vector3& direction) : origin(origin), direction(direction), time(0) {};
-ray::ray(const point3& origin, const vector3& direction, double time) : origin(origin), direction(direction), time(time) {};
+ray::ray(const point3& origin, const vector3& direction, double time) : origin(origin), direction(direction), time(time) {}
 
 point3 ray::at(const double& dist) const
 {
