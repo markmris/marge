cdae924893377da0c5adaeba95c6c3069ecafd3e
markmris <mark.mrishih@gmail.com>
2026-08-14 20:06:31 -0300

    Define boundingbox constructors

---
+++ MARGE/src/includes/boundingbox.cpp
@@ -0,0 +1,11 @@
+#include "boundingbox.h"
+
+boundingbox::boundingbox() {}
+boundingbox::boundingbox(const interval& x, const interval& y, const interval& z) : x(x), y(y), z(z) {}
+
+boundingbox::boundingbox(const point3& a, const point3& b)
+{
+	x = interval(std::fmin(a.x, b.x), std::fmax(a.x, b.x));
+	y = interval(std::fmin(a.y, b.y), std::fmax(a.y, b.y));
+	z = interval(std::fmin(a.z, b.z), std::fmax(a.z, b.z));
+}
