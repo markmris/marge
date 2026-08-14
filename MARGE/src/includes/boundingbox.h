8eab4f9453cea8226073761ea9a4db6af6acb158
markmris <mark.mrishih@gmail.com>
2026-08-14 20:06:07 -0300

    Create initial boundingbox header

---
+++ MARGE/src/includes/boundingbox.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "marge.h"
+
+struct boundingbox
+{
+	interval x, y, z;
+
+	boundingbox();
+	boundingbox(const interval& x, const interval& y, const interval& z);
+	boundingbox(const point3& a, const point3& b);
+
+	const interval axisInterval(int n) const;
+
+	bool hit(const ray& r, interval rayT) const;
+};
