1145b247f4c008558add2f494b70d20470d64f06
markmris <mark.mrishih@gmail.com>
2026-08-14 18:11:27 -0300

    Create randomColor methods

--- MARGE/src/includes/color.h
+++ MARGE/src/includes/color.h
@@ -6,3 +6,5 @@ using color3 = vector3;
 
 void writeColor(std::ostream& out, const color3 &color);
 double linearToGamma(double linearComponent);
+color3 randomColor();
+color3 randomColor(double min, double max);
