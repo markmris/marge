dd2c3027fe99cce3a44ef9b6f599e7240b3e71e7
markmris <mark.mrishih@gmail.com>
2026-08-15 19:38:13 -0300

    Define bvhnode::bvhnode(objects, start, end) constructor

--- MARGE/src/includes/bvh.cpp
+++ MARGE/src/includes/bvh.cpp
@@ -2,6 +2,36 @@
 
 bvhnode::bvhnode(objectlist list) : bvhnode(list.objects, 0, list.objects.size()) {};
 
+bvhnode::bvhnode(std::vector<shared_ptr<hittable>>& objects, size_t start, size_t end)
+{
+	int axis = randomInt(0, 2);
+
+	auto comparator = (axis == 0) ? compareBoxX :
+		(axis == 1) ? compareBoxY : compareBoxZ;
+
+	size_t objectSpan = end - start;
+
+	if (objectSpan == 1)
+	{
+		left = right = objects[start];
+	}
+	else if (objectSpan == 2)
+	{
+		left = objects[start];
+		right = objects[start + 1];
+	}
+	else
+	{
+		std::sort(std::begin(objects) + start, std::begin(objects) + end, comparator);
+
+		auto middle = start + objectSpan / 2;
+		left = make_shared<bvhnode>(objects, start, end);
+		right = make_shared<bvhnode>(objects, middle, end);
+	}
+
+	bbox = boundingbox(left->getBoundingBox(), right->getBoundingBox());
+}
+
 bool bvhnode::boxCompare(const shared_ptr<hittable>& a, const shared_ptr<hittable>& b, int axisIndex)
 {
 	interval axisIntervalA = a->getBoundingBox().axisInterval(axisIndex);
