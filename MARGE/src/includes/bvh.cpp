d31c5f38749c9d74ac883764fb5b44a21d2b6a6e
markmris <mark.mrishih@gmail.com>
2026-08-15 19:27:57 -0300

    Declare boundingbox comparison methods and define bvhnode::boxCompare()

--- MARGE/src/includes/bvh.cpp
+++ MARGE/src/includes/bvh.cpp
@@ -2,6 +2,14 @@
 
 bvhnode::bvhnode(objectlist list) : bvhnode(list.objects, 0, list.objects.size()) {};
 
+bool bvhnode::boxCompare(const shared_ptr<hittable>& a, const shared_ptr<hittable>& b, int axisIndex)
+{
+	interval axisIntervalA = a->getBoundingBox().axisInterval(axisIndex);
+	interval axisIntervalB = b->getBoundingBox().axisInterval(axisIndex);
+
+	return axisIntervalA.min < axisIntervalB.min;
+}
+
 bool bvhnode::hit(const ray& r, interval rayT, hitdata& hd) const
 {
 	if (!bbox.hit(r, rayT)) return false;
--- MARGE/src/includes/bvh.h
+++ MARGE/src/includes/bvh.h
@@ -9,8 +9,12 @@ struct bvhnode : public hittable
 	bvhnode(objectlist list);
 	bvhnode(std::vector<shared_ptr<hittable>>& objects, size_t start, size_t end);
 
-	bool hit(const ray& r, interval rayT, hitdata& hd) const override;
+	static bool boxCompare(const shared_ptr<hittable>& a, const shared_ptr<hittable>& b, int axisIndex);
+	static bool compareBoxX(const shared_ptr<hittable>& a, const shared_ptr<hittable>& b);
+	static bool compareBoxY(const shared_ptr<hittable>& a, const shared_ptr<hittable>& b);
+	static bool compareBoxZ(const shared_ptr<hittable>& a, const shared_ptr<hittable>& b);
 
+	bool hit(const ray& r, interval rayT, hitdata& hd) const override;
 	boundingbox getBoundingBox() const override;
 
 private:
