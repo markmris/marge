fde471b602a72fbceebf9878b66ed7815910390a
markmris <mark.mrishih@gmail.com>
2026-08-16 19:17:01 -0300

    Readd missing randomColor method

--- MARGE/src/includes/color.cpp
+++ MARGE/src/includes/color.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cmath>
 #include "color.h"
+#include "marge.h"
 
 void writeColor(std::ostream& out, const color3& color)
 {
@@ -17,4 +18,17 @@ double linearToGamma(double linearComponent)
         return std::sqrt(linearComponent);
 
     return 0;
+}
+
+color3 randomColor()
+{
+    return color3(randomDouble(0, 1), randomDouble(0, 1), randomDouble(0, 1));
+}
+
+color3 randomColor(double min, double max)
+{
+    if (min < 0) min *= -1;
+    if (max < 0) max *= -1;
+
+    return color3(randomDouble(min, max), randomDouble(min, max), randomDouble(min, max));
 }
