53e7220442965e16b3bf86ca9e14319336cb9e17
markmris <mark.mrishih@gmail.com>
2026-08-18 20:03:06 -0300

    Create checkertexture struct

--- MARGE/src/includes/texture.h
+++ MARGE/src/includes/texture.h
@@ -5,12 +5,32 @@
 struct texture
 {
 	virtual ~texture() = default;
+
+	virtual color3 value(const double horizontalTexture, const double verticalTexture, const point3& point) const = 0;
 };
 
-class solidcolor : public texture
+struct solidcolor : public texture
 {
+private:
 	color3 albedo;
 
+public:
 	solidcolor(const color3& albedo);
 	solidcolor(const double red, const double green, const double blue);
+
+	color3 value(const double horizontalTexture, const double verticalTexture, const point3& point) const override;
+};
+
+struct checkertexture : public texture
+{
+private:
+	double scale;
+	shared_ptr<texture> even;
+	shared_ptr<texture> odd;
+
+public:
+	checkertexture(double scale, shared_ptr<texture> even, shared_ptr<texture> odd);
+	checkertexture(double scale, const color3& c1, const color3& c2);
+
+	color3 value(const double horizontalTexture, const double verticalTexture, const point3& point) const override;
 };
